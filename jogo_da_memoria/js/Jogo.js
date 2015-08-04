c3fae6746de5166e038b9235d15c62c53cff79b4
Pablo Henrick Diniz <phydokz@hotmail.com>
2015-08-04 09:15:03 -0400

    matriz randomica

--- jogo_da_memoria/js/Jogo.js
+++ jogo_da_memoria/js/Jogo.js
@@ -8,7 +8,7 @@ function Jogo() {
 Jogo.prototype.iniciar = function (element) {
     var self = this;
     var baralho = self.baralho;
-    self.render(element,baralho);
+    self.render(element, baralho);
 };
 
 Jogo.prototype.carregar = function (url, callback) {
@@ -18,18 +18,19 @@ Jogo.prototype.carregar = function (url, callback) {
         type: 'post',
         dataType: 'json',
         success: function (baralho) {
-            self.matrizCartao = math.zeros(self.dificuldade + 1,self.dificuldade +1 );
+            self.matrizCartao = math.zeros(self.dificuldade + 1, self.dificuldade + 1);
             self.baralho = new Baralho(baralho);
-            var max = (self.dificuldade+1) * (self.dificuldade + 1);
+            var max = (self.dificuldade + 1) * (self.dificuldade + 1);
             var count = 0;
             var pares = self.baralho.getPares();
-            pares.forEach(function(par){
-                if(count < max-1){
+            pares.forEach(function (par) {
+                if (count < max - 1) {
                     self.insert(par.cartaoA);
                     self.insert(par.cartaoB);
-                    count+=2;
+                    count += 2;
                 }
             });
+            self.suffleMatrix();
             callback.apply(self);
         },
         error: function () {
@@ -43,15 +44,15 @@ Jogo.prototype.loadCartoes = function (baralho, jogo) {
 };
 
 
-Jogo.prototype.insert = function(cartao){
+Jogo.prototype.insert = function (cartao) {
     var position = this.getMatrixFreePosition();
     var self = this;
-    if(position != null){
+    if (position != null) {
         self.matrizCartao = math.subset(self.matrizCartao, math.index(position[0], position[1]), cartao);
     }
 };
 
-Jogo.prototype.getMatrixFreePosition = function(){
+Jogo.prototype.getMatrixFreePosition = function () {
     var index = null;
     var self = this;
 
@@ -59,10 +60,10 @@ Jogo.prototype.getMatrixFreePosition = function(){
     var cols = self.matrizCartao['_size'][1];
 
     for (var x = 0; x < rows; x++) {
-        for (var y = 0; y < cols; y++){
-            var cartao = math.subset(self.matrizCartao,math.index(x,y));
-            if(cartao == 0){
-                index = [x,y];
+        for (var y = 0; y < cols; y++) {
+            var cartao = math.subset(self.matrizCartao, math.index(x, y));
+            if (cartao == 0) {
+                index = [x, y];
                 break;
             }
         }
@@ -71,18 +72,37 @@ Jogo.prototype.getMatrixFreePosition = function(){
     return index;
 };
 
-Jogo.prototype.countCartoes = function(){
+Jogo.prototype.suffleMatrix = function () {
+    var self = this;
+    var rows = self.matrizCartao['_size'][0];
+    var cols = self.matrizCartao['_size'][1];
+
+
+    var matriz = self.matrizCartao;
+    for (var x = 0; x < rows; x++) {
+        for (var y = 0; y < cols; y++) {
+            var rand_x = parseInt(Math.floor(Math.random() * rows));
+            var rand_y = parseInt(Math.floor(Math.random() * cols));
+            var cartao = math.subset(matriz, math.index(x, y));
+            matriz = math.subset(matriz, math.index(x,y),math.subset(matriz,math.index(rand_x,rand_y)));
+            matriz = math.subset(matriz,math.index(rand_x,rand_y),cartao);
+        }
+    }
+    self.matrizCartao = matriz;
+};
+
+Jogo.prototype.countCartoes = function () {
     var count = 0;
     var self = this;
-    self.matrizCartao.forEach(function(value){
-        if(value != 0){
+    self.matrizCartao.forEach(function (value) {
+        if (value != 0) {
             count++;
         }
     });
     return count;
 };
 
-Jogo.prototype.render = function (element,opcoes) {
+Jogo.prototype.render = function (element, opcoes) {
     $(element).empty();
     var self = this;
 
@@ -90,13 +110,13 @@ Jogo.prototype.render = function (element,opcoes) {
     var cols = self.matrizCartao['_size'][1];
 
     for (var x = 0; x < rows; x++) {
-        for (var y = 0; y < cols; y++){
+        for (var y = 0; y < cols; y++) {
             var slot = document.createElement('div');
             slot.className = 'slot';
             $(slot).width(opcoes.largura);
             $(slot).height(opcoes.altura);
             $(element).append(slot);
-            var cartao = math.subset(self.matrizCartao,math.index(x,y));
+            var cartao = math.subset(self.matrizCartao, math.index(x, y));
             if (cartao != null) {
                 $(slot).append(cartao.elemento);
             }
