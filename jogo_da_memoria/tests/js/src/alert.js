90b1cc1f1ac24450694be644030ef942e4d5a403
Pablo Henrick Diniz <phydokz@hotmail.com>
2015-08-12 20:49:44 -0400

    react tests

--- jogo_da_memoria/tests/js/src/alert.js
+++
@@ -1,9 +0,0 @@
-var Alert = React.createClass({
-    render:function(){
-        return (
-            <div className={'alert alert-'+this.props.type} style={{display:this.props.show?'block':'none'}}>
-                {this.props.message}
-            </div>
-        );
-    }
-});
